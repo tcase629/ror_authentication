cd1f6169ec0a1369d055968594d08def0fdfb886
Tyler Case <tcase629@yahoo.com>
2021-10-01 07:10:18 -0600

    added navbar

---
+++ client/src/components/shared/Navbar.js
@@ -0,0 +1,53 @@
+import { Link, withRouter } from 'react-router-dom';
+import { AuthConsumer } from '../../providers/AuthProvider';
+
+const Navbar = ({ user, handleLogout, history }) => {
+
+  const rightNavItems = () => {
+    if (user) {
+      // links if the user is logged in
+      return (
+        <>
+          <li onClick={() => handleLogout(history)}>
+            Logout
+          </li>
+        </>
+      )
+    } else {
+      // links if there is no user logged in 
+      return(
+        <>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+          <Link to="/register">
+            <li>Register</li>
+          </Link>
+        </>
+      )
+    }
+  }
+  return(
+    <>
+      <nav>
+        <ul>
+          {/* links regardless of login status */}
+          <Link to="/">
+            <li>
+              Home
+            </li>
+          </Link>
+          { rightNavItems() }
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+const ConnectedNavbar = (props) => (
+  <AuthConsumer>
+    { value => <Navbar {...value}  {...props}/>}
+  </AuthConsumer>
+)
+
+export default withRouter(ConnectedNavbar)
